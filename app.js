45e51e11631e25227deeaca81a4e478e1e818f92
Nerah Dundas <nerahdas@gmail.com>
2020-06-09 17:32:13 +0200

    ani fix

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
+gsap.registerPlugin(ScrollTrigger);
 
-const slider = document.querySelector(".slider");
 let navMenu = document.querySelector('.navMenu');
 
-//  ----------------------------------  LOGO/SPLASH PAGE Animation---------------------------------------
+// splash animation
 const splash = document.querySelector(".splash");
 const splashImg = document.querySelector(".splashImg");
 
-TweenMax.to(splash, 1, {delay: 1, top: "-100%", ease: "power2.out" })
-TweenMax.to(splashImg, 0.5, {delay: 0.8, opacity: 0, y: -500, ease: "power2.out"  })
+TweenMax.to(splash, 1, {delay: 1, top: "-100%", ease: "power2.out" });
+TweenMax.to(splashImg, 0.5, {delay: 0.8, opacity: 0, y: -500, ease: "power2.out"  });
+
+
+//Hero page l
+gsap.timeline({defaults:{ease:"power2.out" , duration:2}})
+    .from(".hero-image-l h1",{delay: 1, x: -100, transformOrigin:"right" })
+    .from(".subTitle", {delay: 0.1, opacity: 0 });
+
+//section one l
+let tlOne = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".pageOne",
+        start: "top center",
+        markers: true,
+        // toggleActions: "restart none none none"
+    }
+});
+tlOne.from(".pageOne h2", 1, {scale:0, opacity: 0, transformOrigin: "bottom ", ease:"power2.out"});
+   
+    
+ //PAGE TWO  
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".pageTwo",
+        start: "top center",
+        markers: true,
+        toggleActions: "restart none none none"
+    }
+});
+tl.from(".projects", 1,{delay: 0.2 , y: 100,  opacity: 0 , stagger: 0.6, ease: "circ.out" })
+    .from(".viewMore", {delay: 0.3, opacity: 0})
+    .from(".info",{delay: 0.6, x: -100, opacity: 0 , ease: "power2.out" });
+
 
 //  ----------------------------------  NAVIGATION BAR SECTION ---------------------------------------
 let navBtn = document.getElementById('iconBars');
 navBtn.addEventListener('click', openMenu);
 
-function openMenu(){
+function openMenu() {
     navMenu.style.display = "flex";
     document.body.style.overflow = 'hidden';
 
     //-- Opening Animations --//
     TweenMax.from(".navMenu", 0.5, {delay: 0, y: "-100%", transformOrigin:"top", ease: "Expo.out" });
-    TweenMax.from(".navLinks", 0.5, { delay: 0.3, x: -100, opacity:0, ease: "power2.out" });
+    TweenMax.from(".link", 0.5, { delay: 0.2, x: -100, opacity:0, ease: "power2.out", stagger:0.4 });
     TweenMax.from(".lang p", 0.5, { delay: 0.4, x: -100, opacity:0, ease: "power2.out"});
     TweenMax.from(".socials", 0.5, { delay: 0.5, x: -100, opacity:0, ease: "power2.out" });
 }
-
 
 //-------------------------------------- CLOSE NAVIGATION MENU -------------------------------------
 let closeBtn = document.getElementById('closeIcon');
